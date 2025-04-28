ce0f0d173cd7ffcd017ee4927f23f98d1f1244ba
Tomi Salami <salami.tomie@gmail.com>
2025-04-28 09:52:42 -0400

    project set up

---
+++ WarCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WarCards
+//
+//  Created by Tomi E. Salami on 2025-04-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WarCards/WarCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardsApp.swift
+//  WarCards
+//
+//  Created by Tomi E. Salami on 2025-04-28.
+//
+
+import SwiftUI
+
+@main
+struct WarCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
